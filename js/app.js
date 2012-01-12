1954fc6f8818f83bb0ef8982c9bea1746b395ca0
Manuel Astudillo <manuel@optimalbits.com>
2012-01-12 22:27:10 +0100

    Changes in app.'s to reflect the changes in ginger.route.

--- js/app.js
+++ js/app.js
@@ -1,86 +1,128 @@
-define(['jquery', 'underscore', 'ginger/ginger','showdown'], function($, _, ginger, showdown){
+define(['jquery', 
+        'underscore',
+        'ginger/ginger',
+        'showdown',
+        'js!jade.js',
+        'js!moment.min.js'], function($, _, ginger, showdown){
 
 $(function(){
+var jade = require('jade');
 
 ginger.route.root = '/'
 
 ginger.route.listen(function(req){
   req.get(function(){
   
-    req.anim('fadeOut').render('/jade/main.jade', '/css/main.css').anim('fadeIn');
-  
+    req
+      .enter('fadeIn')
+      .render('/jade/main.jade', '/css/main.css')
+      .exit('fadeOut')
+        
     if(req.isLast()){
       req.redirect('/news');
       return;
     }
-    
+  
     req.get('news', '#content', function(){
-      curl(['text!/data/news.json'], function(d){
-        var data = JSON.parse(d);
-            urls = _.pluck(data, 'url');
-        if(req.isLast()){
-          req.anim('fadeOut');
-          req.load(urls, function (done){
-            for(var i=0, len=req.data.length;i<len;i++){
-              data[i].content = showdown.parse(req.data[i]);
-              data[i].url = req.resourceRoute(data[i].url);
-            }
-            req.data = data;
+      var data, urls;
+      
+      if(req.isLast()){
+        req.custom(function(done){
+          curl(['text!/data/news.json'], function(d){
+            data = JSON.parse(d);
+            req.data = _.pluck(data, 'url');
             done();
           });
-          req.render('/jade/news.jade', '/css/news.css', 'news');
-          req.anim('fadeIn');
-        }else{
-          req.get(':id', '#content', function() {
-            var docUrl = '/data/news/'+req.params.id+'.json'
-            req.anim('fadeOut');
-            req.load(docUrl, function(done){
-              for(var i=0, len=data.length;i<len;i++){
-                if(data[i].url===docUrl){
-                  data[i].content = showdown.parse(req.data)
-                  data[i].url = req.url;
-                  req.data = data[i];
-                  break;
-                }
-              }
+        });
+        
+        req.exit('fadeOut');
+        
+        req.load(function (done){
+          for(var i=0, len=req.data.length;i<len;i++){
+            data[i].content = showdown.parse(req.data[i]);
+            data[i].date = moment(data[i].date).fromNow();
+            data[i].url = req.resourceRoute(data[i].url);
+          }
+          req.data = data;
+          done();
+        });
+        
+        req.render('/jade/news.jade', '/css/news.css', 'news');
+        req.enter('fadeIn');
+      }else{        
+        req.get(':id', '#content', function() {
+          req.custom(function(done){
+            curl(['text!/data/news.json'], function(d){
+              data = JSON.parse(d);
+              req.data = _.pluck(data, 'url');
               done();
             });
-            req.render('/jade/news-detail.jade', '/css/news.css', 'doc');
-            req.anim('fadeIn');
           });
-        }
-      });
+          
+          var docUrl = '/data/news/'+req.params.id+'.json';
+          
+          req.exit('fadeOut');
+          req.load(docUrl, function(done){
+            for(var i=0, len=data.length;i<len;i++){
+              if(data[i].url===docUrl){
+                data[i].content = showdown.parse(req.data)
+                data[i].date = moment(data[i].date).fromNow();
+                data[i].url = req.url;
+                req.data = data[i];
+                break;
+              }
+            }
+            done();
+          });
+          req.render('/jade/news-detail.jade', '/css/news.css', 'doc');
+          req.enter('fadeIn');
+        });
+      }
     });
-
+    
     // About
     req.get('about', '#content', function(){
-      req.anim('fadeOut').render('/jade/about.jade').anim('fadeIn');
+      req.enter('fadeIn').render('/jade/about.jade').exit('fadeOut');
     });
       
     // Products
-    req.get('products', '#content', function(){
+    req.get('products', '#products', function(){
     
       // render submenu.
-    
+      if(req.isLast()){
+        req.enter('fadeIn');
+        req.render('/jade/products.jade', '/css/products.css');
+        req.exit('fadeOut');
+      }
+          
       req.get('castmill','#content', function(){
-        req.anim('fadeOut').render('/jade/products/castmill.jade').anim('fadeIn');
+        req.exit('fadeOut').render('/jade/products/castmill.jade').enter('fadeIn');
       });
          
       req.get('ginger', '#content', function(){
-        req.anim('fadeOut').render('/jade/products/ginger.jade').anim('fadeIn');
+        req.exit('fadeOut').render('/jade/products/ginger.jade').enter('fadeIn');
       });
+    });
+    
+    // Partners
+    req.get('partners', '#content', function(){
+      req.exit('fadeOut').render('/jade/partners.jade').enter('fadeIn');
     });
       
     // Career
     req.get('career', '#content', function(){
-      req.anim('fadeOut').render('/jade/career.jade').anim('fadeIn');
+      req.exit('fadeOut').render('/jade/career.jade').enter('fadeIn');
     });
       
     // Contact
     req.get('contact', '#content', function(){
-      req.anim('fadeOut').render('/jade/contact.jade').anim('fadeIn');
+      req.exit('fadeOut').render('/jade/contact.jade').enter('fadeIn');
     });
     
+    // Copyright
+    req.get('copyright', '#content', function(){
+      req.exit('fadeOut').render('/jade/copyright.jade').enter('fadeIn');
+    });
   })
 });
 });
