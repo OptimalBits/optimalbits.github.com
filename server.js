67da780e05e29e10ac6a49ba81e6ad3e22073be5
Manuel Astudillo <manuel@optimalbits.com>
2018-02-20 15:43:35 +0100

    removed crawle

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var http = require('http');
-var crawlme = require('crawlme');
+//var crawlme = require('crawlme');
 
 var app = express()
+/*
   .use(crawlme({
     cacheSize: 5*1024*1024,
     cacheRefresh: 15*60*1000 //every 15 minutes
-  }))
+  }))*/
   .use(express.static(__dirname + '/public'));
 
 var port = process.env['OPTIMALBITS_PORT'] || 8000;
